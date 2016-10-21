7e08225d41e5685cd70e86cddd350ec3d9693984
Rafael Mello <merorafael@gmail.com>
2016-10-21 00:38:19 -0200

    Strategy test changed

--- tests/Mero/Monolog/Handler/StrategyTest.php
+++ tests/Mero/Monolog/Handler/StrategyTest.php
@@ -57,66 +57,13 @@ class StrategyTest extends \PHPUnit_Framework_TestCase
         ]);
     }
 
-    public function dataProviderParameterNotFoundException()
-    {
-        return [
-            [
-                [
-                    'path' => '@app/runtime/logs/log_'.date('Y-m-d').'.log',
-                ],
-                [
-                    'type' => 'stream',
-                ],
-                [
-                    'type' => 'gelf',
-                ],
-                [
-                    'type' => 'rotating_file',
-                    'max_files' => 5,
-                ],
-                [
-                    'type' => 'stream',
-                ],
-                [
-                    'type' => 'yii_db',
-                    'table' => 'logs',
-                ],
-                [
-                    'type' => 'yii_mongo',
-                    'collection' => 'logs',
-                ],
-                [
-                    'type' => 'hipchat',
-                    'token' => 'XXXX',
-                ],
-                [
-                    'type' => 'hipchat',
-                    'room' => 'XXXX',
-                ],
-                [
-                    'type' => 'slack',
-                    'token' => 'XXXX',
-                ],
-                [
-                    'type' => 'slack',
-                    'channel' => 'XXXX',
-                ],
-                [
-                    'type' => 'syslog',
-                ],
-                [
-                    'type' => 'syslogudp',
-                ],
-            ],
-        ];
-    }
-
     /**
-     * @dataProvider dataProviderParameterNotFoundException
      * @expectedException \Mero\Monolog\Exception\ParameterNotFoundException
      */
-    public function testParameterNotFoundException(array $config)
+    public function testParameterNotFoundException()
     {
-        $this->strategy->createFactory($config);
+        $this->strategy->createFactory([
+            'path' => '@app/runtime/logs/log_'.date('Y-m-d').'.log',
+        ]);
     }
 }
